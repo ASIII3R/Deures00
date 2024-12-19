6bd385fe5a96872cac833785f6244ae44bdf0c86
ASIII3R <apozodorado.cf@iesesteveterradas.cat>
2024-12-19 17:14:46 +0100

    prueba

---
+++ Deures_AsierP_00/src/main/java/com/exercicis/Exercici0002.java
@@ -0,0 +1,26 @@
+package com.exercicis;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercici0002 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Locale localeUS = Locale.US;
+
+        System.out.print("Escriu el valor en Euros: ");
+        String valoreuros = scanner.nextLine();
+        double X = Double.parseDouble(valoreuros.replace(',','.'));
+
+        System.out.print("Escriu la tasa de conversió (ex: 1.25): ");
+        String tasa = scanner.nextLine();
+        double tasafinal = Double.parseDouble(tasa.replace(',','.'));
+
+
+        double Y = X*tasafinal;
+
+        System.out.printf(localeUS, "El valor de %.2f€ són %.2f$%n", X, Y);
+
+        scanner.close();
+    }
+}
