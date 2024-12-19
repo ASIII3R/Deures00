336c5245c1f1627c871014e84d97f8dd700ee0a7
ASIII3R <apozodorado.cf@iesesteveterradas.cat>
2024-12-19 18:18:30 +0100

    0010 casi acabat

--- Deures_AsierP_00/src/main/java/com/exercicis/Exercici0010.java
+++ Deures_AsierP_00/src/main/java/com/exercicis/Exercici0010.java
@@ -1,38 +1,58 @@
 package com.exercicis;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Exercici0010 {
 
     public static ArrayList<Integer> generarNumerosAleatoris(int quantitat, int min, int max) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return new ArrayList<>();
+        ArrayList<Integer> numeros = new ArrayList<>();
+        Random random = new Random();
+
+        for (Integer i = 0; i < quantitat; i=i+1){
+            numeros.add(random.nextInt(max-min+1)+min);
+        }
+        return numeros;
     }
 
     public static ArrayList<Integer> multiplicarPerDos(ArrayList<Integer> llista) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return new ArrayList<>();
+        ArrayList<Integer> multiplicar = new ArrayList<>();
+        for (Integer num: llista){
+            multiplicar.add(num*2);
+        }
+        return multiplicar;
     }
 
     public static ArrayList<Integer> filtrarImparells(ArrayList<Integer> llista) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return new ArrayList<>();
+        ArrayList<Integer> imparells = new ArrayList<>();
+        for (int num: llista){
+            if (num%2 != 0){
+                imparells.add(num);
+            }
+        }
+        return imparells;
     }
 
     public static ArrayList<Double> dividirPerDos(ArrayList<Integer> llista) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return new ArrayList<>();
+        ArrayList<Double> dividir = new ArrayList<>();
+        for (double num: llista){
+            dividir.add(num/2.0);
+        }
+        return dividir;
     }
 
     public static void main(String[] args) {
+        ArrayList <Integer> llistaAleatoria = generarNumerosAleatoris(10, 0, 99);
+        System.out.println("Llista inicial: "+ llistaAleatoria);
+
+        ArrayList<Integer> llistaMultiplicada = multiplicarPerDos(llistaAleatoria);
+        System.out.println("Llista multiplicada per 2:"+llistaMultiplicada);
+
+        ArrayList<Integer> llistaImparells = filtrarImparells(llistaAleatoria);
+        System.out.println("Llista només amb números imparells: "+llistaImparells);
+
+        ArrayList<Double> llistaDividir = dividirPerDos(llistaAleatoria);
+        System.out.println("Llista imparells dividirs per 2: "+llistaDividir);
 
     }
 }
